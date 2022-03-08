5c376b94692b419195dd1f8689ebaca0151c70fa
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2022-03-08 02:49:44 -0700

    Update worker-001.js

--- js-workers/worker-001.js
+++ js-workers/worker-001.js
@@ -2,7 +2,7 @@ onmessage = function(e) {
   console.log('Message received from main script', e);  
   //var workerResult = URL.createObjectURL(e.data);
   //console.log('Posting message back to main script');
-  for(var i=0; i<=1000000000; i++){
+  for(var i=0; i<=10000000000; i++){
     var a = 9, b = 10
     var c = a + b
   }
